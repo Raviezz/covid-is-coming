f3b1893a8006a33279257bafad7d9a161eddef86
Raviezz <projectworkkmit@gmail.com>
2020-04-10 03:24:32 -0700

    Updates

---
+++ precache-manifest.f64c1299be7e5c4b8b04ff3e3880d39c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5fd17748753c1f1da0c4c4b113b22a",
+    "url": "/covid-is-coming/index.html"
+  },
+  {
+    "revision": "552e4ecd8a57c2576f2b",
+    "url": "/covid-is-coming/static/css/main.013674a4.chunk.css"
+  },
+  {
+    "revision": "a35f24a2d46546a8d4ec",
+    "url": "/covid-is-coming/static/js/2.9e67f9c7.chunk.js"
+  },
+  {
+    "revision": "d14f21e42c09509e44bd25febcf4b2dc",
+    "url": "/covid-is-coming/static/js/2.9e67f9c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "552e4ecd8a57c2576f2b",
+    "url": "/covid-is-coming/static/js/main.b177ff74.chunk.js"
+  },
+  {
+    "revision": "d3bb03f3a314dd5d7d23",
+    "url": "/covid-is-coming/static/js/runtime-main.914e839d.js"
+  }
+]);
