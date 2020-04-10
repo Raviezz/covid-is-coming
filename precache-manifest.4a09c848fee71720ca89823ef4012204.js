929e06224a4d5014e31baaf53a3608121045d5e1
Raviezz <projectworkkmit@gmail.com>
2020-04-10 06:09:09 -0700

    Updates

---
+++ precache-manifest.4a09c848fee71720ca89823ef4012204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4935830e543eaae2666922ae3ea609b",
+    "url": "/covid-is-coming/index.html"
+  },
+  {
+    "revision": "19f470c88e202ddb37d8",
+    "url": "/covid-is-coming/static/css/main.013674a4.chunk.css"
+  },
+  {
+    "revision": "e41d367e808c47087b03",
+    "url": "/covid-is-coming/static/js/2.5cd4e27d.chunk.js"
+  },
+  {
+    "revision": "d14f21e42c09509e44bd25febcf4b2dc",
+    "url": "/covid-is-coming/static/js/2.5cd4e27d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19f470c88e202ddb37d8",
+    "url": "/covid-is-coming/static/js/main.4f1f8ac5.chunk.js"
+  },
+  {
+    "revision": "d3bb03f3a314dd5d7d23",
+    "url": "/covid-is-coming/static/js/runtime-main.914e839d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-is-coming/precache-manifest.edd2d40373cef84d8c2041eb7ad49fa9.js"
+  "/covid-is-coming/precache-manifest.4a09c848fee71720ca89823ef4012204.js"
 );
 
 self.addEventListener('message', (event) => {
